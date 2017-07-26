b2b53698d324a28a0636ea3ff267bd779e79684f
Matthew Muresan <matt_muresan@hotmail.com>
2017-07-26 17:08:45 -0400

    minor changes

--- php/upload.php
+++ php/upload.php
@@ -9,7 +9,7 @@ switch ($mode) {
 	case ("http"):
 		echo '
 			<span class="item-action">Paste a link to an archive of the images. After pressing Upload, the next stage may take time as the images must be downloaded to this server. Please be patient.</span>
-			<span class="item-action">Archives bigger than 100MiB not reccomended. Dropbox links can be process automatically if you paste the shareable link. For other services, please ensure you get a direct link to the file. If you paste the link in your browser, it should ask to "download" that file.</span>
+			<span class="item-action">Archives bigger than 100MiB not reccomended. Dropbox links can be processed automatically if you paste the shareable link. For other services, please ensure you get a direct link to the file. If you paste the link in your browser, it should ask to "download" that file.</span>
 				<form enctype="multipart/form-data" style="display:block; margin:0.5em 5%; width:90%" action="index.php?action=processarchive" method="post">
 			  	<input style="width:100%" type="text" name="filelocation"><br>
 			<span class="item-title">';
